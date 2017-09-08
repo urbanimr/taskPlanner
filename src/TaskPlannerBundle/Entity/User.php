e29ef0b2d08ae9633288f6304fcd3ecd5cb675f7
marcin urbaniak <marcin.urbaniak@gmail.com>
2017-09-08 13:16:45 +0200

    user class

---
+++ src/TaskPlannerBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcinurbaniak
+ * Date: 08.09.2017
+ * Time: 11:05
+ */
+namespace TaskPlannerBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
---
+++ src/TaskPlannerBundle/TaskPlannerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TaskPlannerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TaskPlannerBundle extends Bundle
+{
+}
